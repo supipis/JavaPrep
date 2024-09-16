774665073bb5c66b8aa94bf3929afaa8aac24046
supipis <nirmanialgama@gmail.com>
2024-09-16 15:46:18 +0200

    Initial commit for Spring Boot project

---
+++ src/test/java/com/example/javaPrep/JavaPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaPrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
